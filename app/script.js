c8beaf076326ec7e0e98a43350c59beb21532b79
Ryan Morrin <ryanmorrin@gmail.com>
2015-04-25 08:58:27 +0100

    Change method of generating label colours:
    Move to a much better way of calculating colours which ensures the most
    visually distinguishing colours are chosen.
    
    Credit: http://stackoverflow.com/a/898750

--- app/script.js
+++ app/script.js
@@ -4,31 +4,7 @@
 
   var $subreddits,
       $entryLinks,
-      colours = [
-        '#AB4642',
-        '#DC9656',
-        '#F7CA88',
-        '#A1B56C',
-        '#86C1B9',
-        '#7CAFC2',
-        '#BA8BAF',
-        '#A16946',
-        '#8968CD',
-        '#838EDE',
-        '#7D9EC0',
-        '#2FAA96',
-        '#CECC15',
-        '#8B3A3A',
-        '#A78D84',
-        '#8B814C',
-        '#526F35',
-        '#388E8E',
-        '#5959AB',
-        '#8B8989',
-        '#CD661D',
-        '#CECC15',
-        '#0D4F8B'
-      ],
+      colours = [],
       colourMap = [];
 
   function getUnusedColour() {
@@ -37,31 +13,111 @@
     if (colourMap.filter(function (val) { return val.colour === random; }).length === 0) {
       // colour not previously chosen
       return random;
-    } else if (colourMap.length >= colours.length) {
-      // all colours used, return gray
-      return '#EEEEEE';
     } else {
       // colour already chosen, try different value
       return getUnusedColour();
     }
   }
 
+  // credit to: http://stackoverflow.com/a/8987502
+  function hsvToRgb(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
+
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    s = Math.max(0, Math.min(100, s));
+    v = Math.max(0, Math.min(100, v));
+
+    // We accept saturation and value arguments from 0 to 100 because that's
+    // how Photoshop represents those values. Internally, however, the
+    // saturation and value are calculated from a range of 0 to 1. We make
+    // That conversion here.
+    s /= 100;
+    v /= 100;
+
+    if(s === 0) {
+      // Achromatic (grey)
+      r = g = b = v;
+      return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
+
+    switch(i) {
+      case 0:
+        r = v;
+        g = t;
+        b = p;
+        break;
+
+      case 1:
+        r = q;
+        g = v;
+        b = p;
+        break;
+
+      case 2:
+        r = p;
+        g = v;
+        b = t;
+        break;
+
+      case 3:
+        r = p;
+        g = q;
+        b = v;
+        break;
+
+      case 4:
+        r = t;
+        g = p;
+        b = v;
+        break;
+
+      default: // case 5:
+        r = v;
+        g = p;
+        b = q;
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  }
+
+  // credit to: http://stackoverflow.com/a/8987502
+  function distinctColours(count) {
+    var colours = [];
+    for(var hue = 0; hue < 360; hue += 360 / count) {
+      colours.push(hsvToRgb(hue, 40, 90));
+    }
+    return colours;
+  }
+
   function applyColour($el, subredditColour) {
+    subredditColour = subredditColour.concat([0.6]); //add alpha value
+
     $el.css({
-      'background': subredditColour,
+      'background': 'rgba(' + subredditColour.join(',') + ')',
       'color': '#000',
-      opacity: '0.6'
     });
   }
 
   $(function () {
     $subreddits = $('.multi-details .subreddits li a');
     $entryLinks = $('.entry .tagline .subreddit');
+    colours = distinctColours($subreddits.length);
 
     $subreddits.each(function () {
       var $el = $(this);
 
       var subredditColour = getUnusedColour();
+
       // keep track of colour/subreddit mappings
       colourMap.push({ subreddit: $el.text(), colour: subredditColour });
 
